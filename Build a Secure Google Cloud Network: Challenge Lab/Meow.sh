635111e02b27fa80d45945759ecd64e4e65fd93b
manavyugaitech <manavyugaipromotion@gmail.com>
2026-09-02 14:39:09 +0530

    Update Meow.sh

--- Build a Secure Google Cloud Network: Challenge Lab/Meow.sh
+++ Build a Secure Google Cloud Network: Challenge Lab/Meow.sh
@@ -21,28 +21,57 @@ BG_WHITE=`tput setab 7`
 
 BOLD=`tput bold`
 RESET=`tput sgr0`
+
+#----------------------------------------------------manual input-------------------------------------------#
+
+echo -e "\n${BG_BLUE}${WHITE}${BOLD} CONFIGURATION ${RESET} ${BLUE}${BOLD}Please enter the required environment variables:${RESET}\n"
+
+read -p "${CYAN}Enter IAP_NET_TAG: ${RESET}" IAP_NET_TAG
+read -p "${CYAN}Enter INT_NET_TAG: ${RESET}" INT_NET_TAG
+read -p "${CYAN}Enter HTTP_NET_TAG: ${RESET}" HTTP_NET_TAG
+read -p "${CYAN}Enter ZONE: ${RESET}" ZONE
+
+export IAP_NET_TAG
+export INT_NET_TAG
+export HTTP_NET_TAG
+export ZONE
+
+# Validate inputs
+if [ -z "$IAP_NET_TAG" ] || [ -z "$INT_NET_TAG" ] || [ -z "$HTTP_NET_TAG" ] || [ -z "$ZONE" ]; then
+    echo -e "\n${BG_RED}${WHITE}${BOLD} ERROR ${RESET} ${RED}One or more variables were left empty. Aborting script.${RESET}\n"
+    exit 1
+fi
+
 #----------------------------------------------------start--------------------------------------------------#
 
-echo "${YELLOW}${BOLD}Starting${RESET}" "${GREEN}${BOLD}Execution${RESET}"
+echo -e "\n${BG_CYAN}${BLACK}${BOLD} INFO ${RESET} ${CYAN}${BOLD}Starting GCP Network & Firewall Configuration Execution...${RESET}\n"
 
+echo "${CYAN}[1/7]${RESET} Deleting open-access firewall rule..."
 gcloud compute firewall-rules delete open-access --quiet
 
+echo "${CYAN}[2/7]${RESET} Starting bastion instance..."
 gcloud compute instances start bastion \
     --project=$DEVSHELL_PROJECT_ID \
     --zone=$ZONE
- 
+
+echo "${CYAN}[3/7]${RESET} Creating ssh-ingress firewall rule..."
 gcloud compute firewall-rules create ssh-ingress --allow=tcp:22 --source-ranges 35.235.240.0/20 --target-tags $IAP_NET_TAG --network acme-vpc
- 
+
+echo "${CYAN}[4/7]${RESET} Tagging bastion instance..."
 gcloud compute instances add-tags bastion --tags=$IAP_NET_TAG --zone=$ZONE
- 
+
+echo "${CYAN}[5/7]${RESET} Creating http-ingress firewall rule..."
 gcloud compute firewall-rules create http-ingress --allow=tcp:80 --source-ranges 0.0.0.0/0 --target-tags $HTTP_NET_TAG --network acme-vpc
- 
+
+echo "${CYAN}[6/7]${RESET} Tagging juice-shop instance (HTTP)..."
 gcloud compute instances add-tags juice-shop --tags=$HTTP_NET_TAG --zone=$ZONE
- 
+
+echo "${CYAN}[7/7]${RESET} Creating internal-ssh-ingress rule & tagging juice-shop..."
 gcloud compute firewall-rules create internal-ssh-ingress --allow=tcp:22 --source-ranges 192.168.10.0/24 --target-tags $INT_NET_TAG --network acme-vpc
- 
+
 gcloud compute instances add-tags juice-shop --tags=$INT_NET_TAG --zone=$ZONE
 
+echo -e "\n${YELLOW}⏳ Waiting 30 seconds for network interfaces to stabilize...${RESET}"
 sleep 30
 
 cat > prepare_disk.sh <<'EOF_END'
@@ -53,10 +82,12 @@ gcloud compute ssh juice-shop --internal-ip --zone=$ZONE --quiet
 
 EOF_END
 
+echo "${BLUE}▶ Copying script to bastion host...${RESET}"
 gcloud compute scp prepare_disk.sh bastion:/tmp --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet
 
+echo "${BLUE}▶ Executing configuration script on bastion host...${RESET}"
 gcloud compute ssh bastion --project=$DEVSHELL_PROJECT_ID --zone=$ZONE --quiet --command="bash /tmp/prepare_disk.sh"
 
-echo "${RED}${BOLD}Congratulations${RESET}" "${WHITE}${BOLD}for${RESET}" "${GREEN}${BOLD}Completing the Lab !!!${RESET}"
+echo -e "\n${BG_GREEN}${BLACK}${BOLD} SUCCESS ${RESET} ${GREEN}${BOLD}Congratulations for Completing the Lab !!!${RESET}\n"
 
 #-----------------------------------------------------end----------------------------------------------------------#
